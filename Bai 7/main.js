a139b66f8c56f59c267248587c1a8c5101a9d232
TuanDang03 <96873363+TuanDang03@users.noreply.github.com>
2022-01-06 00:24:37 +0700

    Add files via upload

---
+++ Bai 7/main.js
@@ -0,0 +1,13 @@
+alert("Bài 1");
+let firstnumber = prompt("Số thứ nhất", 1);
+let secondnumber = prompt("Số thứ hai", 2);
+alert("12"); 
+
+alert("Bài 3");
+let text = prompt("Nhập kí tự bất kì");
+alert("Số kí tự là : " + text.length);
+
+//Bai2
+const p = `Hello world!`;
+console.log(p.replace(`world`, `Tuan`));
+
