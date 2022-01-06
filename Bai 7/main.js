311eb39a62039df3f3483eebca0a04c3189250ae
TuanDang03 <96873363+TuanDang03@users.noreply.github.com>
2022-01-06 08:04:45 +0700

    Update main.js

--- Bai 7/main.js
+++ Bai 7/main.js
@@ -1,13 +1,13 @@
-alert("Bài 1");
-let firstnumber = prompt("Số thứ nhất", 1);
-let secondnumber = prompt("Số thứ hai", 2);
-alert("12"); 
+alert(" Bài 1 : ");
+let a = prompt(" Nhập số thứ nhất : ", 1);
+let b = prompt(" Nhập số thứ hai : ", 2);
+alert(" 12 ");
 
-alert("Bài 3");
-let text = prompt("Nhập kí tự bất kì");
-alert("Số kí tự là : " + text.length);
+alert(" Bài 3 : ");
+let text = prompt( "Type your words : ");
+alert(" Số kí tự là : "+ text.length);
+ 
 
 //Bai2
-const p = `Hello world!`;
-console.log(p.replace(`world`, `Tuan`));
-
+let word = "Hello world!"
+console.log(word.replace(`world`, `Tuan`));
