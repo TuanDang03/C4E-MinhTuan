6c1d08cfe781aa8c9ccec006d0e597175f36847e
TuanDang03 <96873363+TuanDang03@users.noreply.github.com>
2022-01-15 13:02:41 +0700

    Update Bai2.js

--- Bai 11/Bai2.js
+++ Bai 11/Bai2.js
@@ -1,27 +1,19 @@
-function sum()
-  {
-      let nut1 = document.getElementById("nut1")
-      let nut2 = document.getElementById("nut2")
-      let nut3 = document.getElementById("nut3")
-      let nut4 = document.getElementById("nut4")
-      let so1 = document.myform.number1.value;
-      let so2 = document.myform.number2.value;
-      let ketqua1 = parseInt(so1) + parseInt(so2);
-      let ketqua2 = parseInt(so1) - parseInt(so2);
-      let ketqua3 = parseInt(so1) * parseInt(so2);
-      let ketqua4 = parseInt(so1) / parseInt(so2);
-    nut1.onclick = () =>{
-      document.getElementById("add").value = ketqua1
-    }
-    nut2.onclick = () =>{
-      document.getElementById("add").value = ketqua2
-    }
-    nut3.onclick = () =>{
-      document.getElementById("add").value = ketqua3
-    }
-    nut4.onclick = () =>{
-      document.getElementById("add").value = ketqua4
-    }
-    
-    
-  }
+let nut1 = document.querySelector("#nut1")
+let nut2 = document.querySelector("#nut2")
+let nut3 = document.querySelector("#nut3")
+let nut4 = document.querySelector("#nut4")
+let so1 = document.querySelector("#so1")
+let so2 = document.querySelector("#so2")
+let result = document.querySelector("#add")
+nut1.onclick = () =>{
+result.value = parseFloat(so1.value) + parseFloat(so2.value)
+}
+nut2.onclick = () =>{
+result.value = parseFloat(so1.value) - parseFloat(so2.value)
+}
+nut3.onclick = () =>{
+result.value = parseFloat(so1.value) * parseFloat(so2.value)
+}
+nut4.onclick = () =>{
+result.value = parseFloat(so1.value) / parseFloat(so2.value)
+}
