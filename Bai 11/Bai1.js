d3a9f83f258ecd421318554169379e233e680eae
TuanDang03 <96873363+TuanDang03@users.noreply.github.com>
2022-02-25 00:03:48 +0700

    Delete Bai 11 directory

--- Bai 11/Bai1.js
+++
@@ -1,7 +0,0 @@
-//BAI 1:
-let background = document.querySelector("#background");
-let backgroundmau = document.querySelector("#backgroundmau");
-
-background.onclick = () => {
-  document.body.style.backgroundColor = backgroundmau.value
-}
