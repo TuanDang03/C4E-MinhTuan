5d3c9ef2418deefe932814090e72822f659ff2ec
TuanDang03 <96873363+TuanDang03@users.noreply.github.com>
2022-02-25 00:03:40 +0700

    Delete Bai 10 directory

--- Bai 10/Bài2.js
+++
@@ -1,9 +0,0 @@
-//BÀI 2:
-let obj = {
-    b: 2,
-    a: 1,
-    c: 3,
-};
-let name = Object.keys(obj)
-let uppercase = name.map(x => x.toUpperCase())
-console.log(uppercase)
--- Bai 10/Bài3.js
+++
@@ -1,9 +0,0 @@
-//BÀI 3:
-let myFamily = {
-    quantity: 4,
-    member: ["Dad", "Mom", "Me", "Daughter"],
-    location: "Vietnam",
-  };
-  let family = {}
-  Object.setPrototypeOf(family, myFamily)
-  console.log(family)
